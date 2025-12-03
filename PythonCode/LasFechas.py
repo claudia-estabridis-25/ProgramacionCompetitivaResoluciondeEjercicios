5e78f17889ceaf3ef4442f69fdeffaa0bd5a30c7
claudia-estabridis-25 <u202013964@upc.edu.pe>
2025-12-03 00:54:31 -0500

    Update LasFechas.py

--- PythonCode/LasFechas.py
+++ PythonCode/LasFechas.py
@@ -1,16 +1,25 @@
 fecha = input()
 
-if fecha[4] == "-" and fecha[7] == "-":
-    digitosAnio = fecha[0:4]
-    digitosMes = fecha[5:7]
-    digitosDia = fecha[8:10]
+digitosAnio = fecha[0:4]
+digitosMes = fecha[5:7]
+digitosDia = fecha[8:10]
 
-    fecList = [digitosDia, digitosMes, digitosAnio]
-    fecPedida = "-".join(fecList)
+fecList = [digitosDia, digitosMes, digitosAnio]
+fecPedida = "-".join(fecList)
 
-    print(fecPedida)
+print(fecPedida)
+
+# Pensé que los inputs contenían también formato DD-MM-AAAA, así que hice una validación, pero esto es innecesario realmente
+# fecha = input()
+# if fecha[4] == "-" and fecha[7] == "-":
+#    digitosAnio = fecha[0:4]
+#    digitosMes = fecha[5:7]
+#    digitosDia = fecha[8:10]
+#    fecList = [digitosDia, digitosMes, digitosAnio]
+#    fecPedida = "-".join(fecList)
+#    print(fecPedida)
 
 # Si hay un guión - después de los dos primeros dígitos [0, 2]
 # Y hay un guión luego de los dos siguientes dígitos [3:5]
-elif fecha[2] == "-" and fecha[5] == "-": #Significa que el formato es DD-MM-AAA
-    print(fecha)
+# elif fecha[2] == "-" and fecha[5] == "-": #Significa que el formato es DD-MM-AAA
+#    print(fecha)
