8ea2c2ff7d0eeea3b92103fec6eab2a389240dfd
claudia-estabridis-25 <u202013964@upc.edu.pe>
2026-03-27 00:07:06 -0500

    Remove MCM formula comment from FestivalDeVelasYFaroles.py

--- ConcursosCoderBloom/ProblemasDiciembre2025/Resolución/FestivalDeVelasYFaroles.py
+++ ConcursosCoderBloom/ProblemasDiciembre2025/Resolución/FestivalDeVelasYFaroles.py
@@ -11,6 +11,4 @@ print(mcm)
 MCD: Busca el número más grande que divide a otros (se usa para repartir cosas en grupos iguales).
 
 MCM: Busca el número más pequeño que es múltiplo de otros (se usa para saber cuándo coinciden eventos que se repiten).
-
-$$MCM(a, b) = \frac{|a \times b|}{MCD(a, b)}$$
 """
